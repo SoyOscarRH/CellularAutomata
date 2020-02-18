9a2e8662b2ea4d82f8aaaa74bb5fa4668443e524
SoyOscarRH <soyoscarrh@gmail.com>
2020-02-18 00:42:03 -0600

    Revert "Merge branch 'master' of https://github.com/SoyOscarRH/CellularAutomata"
    This reverts commit 65854ae42069752a80c1d3f9e648d7fe17f3589e, reversing
    changes made to 987cc7ea265e5585182228b832488d09c77ffcc4.

---
+++ src/automata.ts
@@ -0,0 +1,17 @@
+const getRules = (rules_id: number) => {
+  return (s: Array<number>, i: number) => {
+    const limit = s.length - 1;
+    const n1 = i === 0 ? limit : i - 1;
+    const n2 = i === limit ? 0 : i + 1;
+
+    const id = (s[n1] << 2) + (s[i] << 1) + (s[n2] << 0);
+    return (rules_id >> id) & 1;
+  };
+};
+
+const getLine = (space: Array<number>, rules_id: number) => {
+  const rules = getRules(rules_id);
+  return space.map((_, i) => rules(space, i));
+};
+
+export { getLine };
