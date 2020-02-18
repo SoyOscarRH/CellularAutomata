ce9b350b853231623577a2d14f2bf69752dfd5de
SoyOscarRH <soyoscarrh@gmail.com>
2020-02-18 00:37:13 -0600

    Updates

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "./index.css";
-import App from "./App";
-
-const DOM_NODE = document.getElementById("root");
-ReactDOM.render(<App />, DOM_NODE);
