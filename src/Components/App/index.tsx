56e2921c16b42c3ac2f7d3979739b95c6519beae
SoyOscarRH <soyoscarrh@gmail.com>
2020-03-03 16:19:59 -0600

    Add better simple and advance

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -1,3 +1,39 @@
-import App from "./App"
+import React from "react";
 
-export default App
+import { useToggle } from "../../Hooks/useToggle";
+
+import Simple from "./Simple";
+import Advance from "./Advance";
+
+import stylesApp from "./App.module.css";
+import stylesGame from "./Game.module.css";
+
+const App: React.FC = () => {
+  const [simpleMode, toggleMode] = useToggle(true);
+
+  const style = {backgroundColor: `rgba(255, 255, 255, ${simpleMode ? 0.7 : 0.1})`};
+  const props = { className: stylesGame.intenseButton, style };
+
+  const Game = simpleMode? <Simple /> : <Advance />
+
+  return (
+    <div className={stylesApp.app}>
+      <main className={stylesApp.appMain}>
+        <h2>Cellular Automata</h2>
+
+        <section className={stylesGame.segment}>
+          <div className={stylesGame.container}>
+            <button onClick={toggleMode} {...props}>
+              Simple Mode {simpleMode ? "on" : "off"}
+            </button>
+          </div>
+        </section>
+
+        {Game}
+
+      </main>
+    </div>
+  );
+};
+
+export default App;
